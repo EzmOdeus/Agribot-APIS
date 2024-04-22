ba5fee3677b2ae26b520139392b5474a4597e8b4
EzmOdeus <shafrax500@gmail.com>
2024-04-22 17:59:09 +0200

    V1

---
+++ src/api/sensor1/controllers/sensor1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sensor1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sensor1.sensor1');
---
+++ src/api/sensor1/routes/sensor1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sensor1 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sensor1.sensor1');
